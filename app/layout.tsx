9d4649e3f63f6b4fc80c5c4c98ab0555cfaa0c42
Mani Teja <maniteja111@gmail.com>
2026-06-07 09:55:40 +0530

    fix: Remove old LoyaltyPopup component with indigo button

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CartProvider } from "./contexts/CartContext";
-import LoyaltyPopup from "@/components/LoyaltyPopup";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +31,6 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <CartProvider>
           {children}
-          <LoyaltyPopup />
         </CartProvider>
       </body>
     </html>
